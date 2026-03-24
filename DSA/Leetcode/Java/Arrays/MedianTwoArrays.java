d65a8977bc0f0238411dc86bbdd945b68458e7da
Shailender Tiwari <shailendert@users.noreply.github.com>
2026-03-24 21:25:23 +0530

    Update MedianTwoArrays.java
    // First solution brute-force, finding a solution first before optimize

--- DSA/Leetcode/Java/Arrays/MedianTwoArrays.java
+++ DSA/Leetcode/Java/Arrays/MedianTwoArrays.java
@@ -1,3 +1,24 @@
 // Problem Statement
 // Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
 // The overall run time complexity should be O(log (m+n)).
+
+// First solution brute-force, finding a solution first before optimize
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] combinedArray = new int[nums1.length + nums2.length];
+        System.arraycopy(nums1, 0, combinedArray, 0, nums1.length);
+        System.arraycopy(nums2, 0, combinedArray, nums1.length, nums2.length);
+        int combinedArrLength = combinedArray.length;
+        Arrays.sort(combinedArray);
+        if (combinedArrLength % 2 != 0)
+        {
+            return combinedArray[combinedArrLength / 2];
+        }
+        else
+        {
+            double firstMedian = combinedArray[combinedArrLength / 2];
+            double secMedian = combinedArray[combinedArrLength / 2 - 1];
+            return (firstMedian + secMedian)  / 2;
+        }
+    }
+}
